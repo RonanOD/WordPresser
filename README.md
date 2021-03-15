f22637e9a44ccff44fca8f619c022b61f7dfd84d
Ronan O'Driscoll <ronanodriscoll@gmail.com>
2021-03-15 14:47:31 -0300

    caching of token functioning and sites list

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # WordPresser
 
-Query the stats of all of your WordPress sites from the command line.
+Little utility to query the stats of all of your WordPress sites from the command line. Drops a pid file of your OAuth2 access token. To reauthenticate, delete the `.token` file.
 
-Needs:
+## Features:
+ * OAuth2
+ * File IO
+ * HTTP Calls
+ * JSON and REST handling
+
+
+## Needs:
  * Oauth2 Authentication set up. Follow details [here](https://developer.wordpress.com/docs/oauth2/)
--- wpr.go
+++ wpr.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -28,17 +29,24 @@ type Stat struct {
 	ViewsYesterday    int `json:"views_yesterday"`
 }
 
+// Global list of sites
+type AllSites struct {
+	Sites []Site `json:"sites"`
+}
+
+type Site struct {
+	ID  int    `json:"ID"`
+	URL string `json:"URL"`
+}
+
 // use godot package to load/read the .env file and
 // return the value of the key
 func goDotEnvVariable(key string) string {
-
 	// load .env file
 	err := godotenv.Load(".env")
-
 	if err != nil {
 		log.Fatalf("Error loading .env file")
 	}
-
 	return os.Getenv(key)
 }
 
@@ -71,7 +79,11 @@ func oauthCall() *http.Client {
 		log.Fatal(err)
 	}
 	tok, err := conf.Exchange(ctx, code)
-	// The ACCESS TOKEN. fmt.Println(tok.AccessToken)
+	// Write out the token so we can reuse token next time.
+	io_err := ioutil.WriteFile(".token", []byte(tok.AccessToken), 0644)
+	if io_err != nil {
+		log.Fatal(io_err)
+	}
 
 	if err != nil {
 		log.Fatal(err)
@@ -114,9 +126,41 @@ func getStats(domain string, http_client *http.Client) (*Sites, error) {
 }
 
 func main() {
-	// initialize a client with OAuth
 	domain := "poor.farm"
-	http_client := oauthCall()
+	token_file := ".token"
+	var http_client *http.Client
+	if _, err := os.Stat(token_file); os.IsNotExist(err) {
+		// initialize a client with OAuth. Drops .token for next time.
+		http_client = oauthCall()
+	} else {
+		// load a client with bearer token
+		content, err := ioutil.ReadFile(token_file)
+		if err != nil {
+			log.Fatalf("error: %s", err)
+		}
+		// Convert []byte to string and print to screen
+		token := string(content)
+		http_client = &http.Client{}
+		req, _ := http.NewRequest("GET", "https://public-api.wordpress.com/rest/v1/me/sites", nil)
+		req.Header.Set("authorization", fmt.Sprintf("Bearer %s", token))
+		res, err := http_client.Do(req)
+		if err != nil {
+			log.Fatalf("error: %s", err)
+			return
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode == http.StatusOK {
+			sites := &AllSites{}
+			dec := json.NewDecoder(res.Body)
+			if err := dec.Decode(sites); err != nil {
+				log.Fatalf("error: %s", err)
+			}
+			fmt.Printf("%+v\n", sites)
+		}
+		return
+	}
+
 	stats, err := getStats(domain, http_client)
 
 	if err != nil {
